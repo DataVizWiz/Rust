6925f705d6ad32c003708fef95de77f494b74781
DataVizWiz <metin@alisho.co.uk>
2024-09-08 12:43:56 +0100

    Adding practice for error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{Error, BufReader, BufRead};
+
+
+fn read_file() -> Result<File, Error> {
+    // Propogate errors higher up the call stack
+    let result = File::open("numbers.txt")?;
+    Ok(result)
 }
+
+fn main() {
+    let mut vec: Vec<i32> = Vec::new();
+
+    match read_file() {
+        Ok(x) => {
+            for line in BufReader::new(x)
+                .lines() {
+                    let content = line.unwrap();
+
+                    match content.parse::<i32>() {
+                        Ok(n) => vec.push(n),
+                        Err(e) => println!("Invalid line: {e}"),
+                     }
+                }
+        },
+        Err(e) => println!("Error opening file: {e:?}.")
+    };
+
+    println!("{:?}", vec);
+}
+
+// fn main() {
+
+
+//     for i in file.it {
+//         println!(i);
+//     } 
+// }
