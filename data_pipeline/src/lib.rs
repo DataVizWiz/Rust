4f26cd21ccc260a565b4d7caf7dc590a1c6b649c
DataVizWiz <metin@alisho.co.uk>
2024-10-13 13:08:31 +0100

    movies project replaced with data_pipeline

--- data_pipeline/src/lib.rs
+++ data_pipeline/src/lib.rs
@@ -1,2 +1,2 @@
-pub mod data;
-pub mod processing;
+pub mod movies;
+pub mod utils;
--- data_pipeline/src/main.rs
+++ data_pipeline/src/main.rs
@@ -1,34 +1,30 @@
-use data_pipeline::data::{DataEntry, DataValue};
-use data_pipeline::processing::{filter_entries, sum_integer_values};
+use data_pipeline::movies::metadata::Movie;
+use data_pipeline::movies::ratings::Rating;
 
 fn main() {
-    let entries = vec![
-        DataEntry::new(1, DataValue::Integer(42), "sensor1", "2024-08-31T12:00:00", vec!["temperature".to_string(), "indoors".to_string()]),
-        DataEntry::new(2, DataValue::Text("hello world".to_string()), "sensor2", "2024-08-31T12:10:00", vec!["greeting".to_string()]),
-    ];
+    let mut movies: Vec<Movie> = Movie::as_vec();
+    let ratings_map = Rating::as_map();
 
-    // Attempt to filter entries (immutable borrow)
-    let filtered_entries = filter_entries(&entries, &|value| match value {
-        DataValue::Integer(x) => *x > 10,
-        _ => false,
-    });
-
-    // Attempt to transform text (mutable borrow)
-    // transform_text_to_uppercase(&mut entries);
-
-    // Attempt to calculate sum of integers (immutable borrow)
-    let sum = sum_integer_values(&entries);
-
-    // Display results
-    println!("Filtered Entries:");
-    for entry in &filtered_entries {
-        entry.display();
+    //To modify the ratings field you need a mutable reference to each Movie in the vector
+    for movie in &mut movies {
+        // Use if let for optional handling. Dereference the values using &
+        if let Some(&rating) = ratings_map.get(&movie.id) {
+            movie.rating = Some(rating);
+        } else {
+            movie.rating = None;
+        }
     }
 
-    println!("\nTransformed Entries:");
-    for entry in &entries {
-        entry.display();
-    }
+    let fv_star: Vec<&Movie> = movies
+        .iter()
+        .filter(|&movie| movie.rating == Some(5.0))
+        .collect();
 
-    println!("\nSum of integer values: {}", sum);
+    for i in fv_star {
+        if i.genres.contains("Romance") {
+            println!("{:#?}", i.original_title);
+            println!("{:#?}", i.genres);
+            println!("{:#?}", i.overview);
+        }
+    }
 }
